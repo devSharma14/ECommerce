f34c1d63e3b8f16ac384109e819e4a8f14ecae47
devSharma14 <148771436+devSharma14@users.noreply.github.com>
2024-04-23 00:40:25 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,29 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header"; // Adjusted import path
-import Cards from "./components/Cards"; // Import the Cards component\
-import products from "./ProductData";
-import Footer from "./components/Footer";
-import Cart from "./components/Cart";
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Cards from './components/Cards';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart'; // Import Cart component
+import Aurat from './components/Aurat';
 
 function App() {
-  // Array of different product details
-
   return (
-    <div>
-      <Header />
-
-      {/* Pass the products array to the Cards component */}
-      <Cards products={products} />
-      {/* <Product_description></Product_description> */}
-      {/* <Footer></Footer> */}
-      <Footer />
-
-      {/* <Cart /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header />
+              <Aurat/>
+              <Cards />
+            </div>
+          }
+        />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/cart/:productId" element={<Cart />} /> {/* Update the route for Cart */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
